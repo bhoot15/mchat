538d53469c7ca1433510085671af2c1dc832f99b
xubi <shoumikxbox@gmail.com>
2018-03-07 18:44:25 +0600

    spin kit added

--- app/src/main/java/com/marceme/marcefirebasechat/ui/ChatMainActivity.java
+++ app/src/main/java/com/marceme/marcefirebasechat/ui/ChatMainActivity.java
@@ -6,11 +6,15 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.github.ybq.android.spinkit.style.MultiplePulse;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
@@ -98,7 +102,7 @@ public class ChatMainActivity extends Activity {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
 
-                hideProgressBarForUsers();
+                //hideProgressBarForUsers();
                 FirebaseUser user = firebaseAuth.getCurrentUser();
 
                 if (user != null) {
@@ -189,6 +193,30 @@ public class ChatMainActivity extends Activity {
     }
 
     private void showProgressBarForUsers() {
+        ProgressBar progressBar = (ProgressBar) findViewById(R.id.spin_kit);
+        progressBar.setVisibility(View.VISIBLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
+                WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+        MultiplePulse multiplePulse = new MultiplePulse();
+        progressBar.setIndeterminateDrawable(multiplePulse);
+    }
+
+    private void hideProgressBarForUsers() {
+        try {
+            ProgressBar progressBar = (ProgressBar) findViewById(R.id.spin_kit);
+            progressBar.setVisibility(View.GONE);
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+        }catch (Exception e){
+            Log.e("ex ", String.valueOf(e));
+            /*Snackbar snackbar = Snackbar
+                    .make(mUsersRecyclerView, "No Internet Connection", Snackbar.LENGTH_SHORT);
+
+            snackbar.show();*/
+            Toast.makeText(this, "No Internet Connection", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /*private void showProgressBarForUsers() {
         mProgressBarForUsers.setVisibility(View.VISIBLE);
     }
 
@@ -196,7 +224,7 @@ public class ChatMainActivity extends Activity {
         if (mProgressBarForUsers.getVisibility() == View.VISIBLE) {
             mProgressBarForUsers.setVisibility(View.GONE);
         }
-    }
+    }*/
 
     private ChildEventListener getChildEventListener() {
         return new ChildEventListener() {
